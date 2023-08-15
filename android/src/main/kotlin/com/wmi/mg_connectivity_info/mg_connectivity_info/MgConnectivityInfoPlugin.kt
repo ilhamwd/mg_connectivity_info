2f17eab09b9bd1849c084e87da55f4dba45d09b8
iilhamwd <iilhamwd@gmail.com>
2023-08-15 17:02:14 +0700

    fix/unitialized activity variable

--- android/src/main/kotlin/com/wmi/mg_connectivity_info/mg_connectivity_info/MgConnectivityInfoPlugin.kt
+++ android/src/main/kotlin/com/wmi/mg_connectivity_info/mg_connectivity_info/MgConnectivityInfoPlugin.kt
@@ -13,17 +13,29 @@ import androidx.core.app.ActivityCompat.requestPermissions
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodChannel
 
 class MgConnectivityInfoPlugin() : FlutterPlugin, ActivityAware {
     private lateinit var activity: Activity
     private lateinit var channel: MethodChannel
     private lateinit var context: Context
+    private lateinit var binaryMessenger: BinaryMessenger
     private val requiredPermissions = arrayOf(Manifest.permission.READ_PHONE_STATE);
 
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         channel = MethodChannel(flutterPluginBinding.binaryMessenger, "mg_connectivity_info")
         context = flutterPluginBinding.applicationContext
+        binaryMessenger = flutterPluginBinding.binaryMessenger
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activity = binding.activity
+
         for (permission in requiredPermissions) {
             if (ActivityCompat.checkSelfPermission(
                     context,
@@ -34,16 +46,8 @@ class MgConnectivityInfoPlugin() : FlutterPlugin, ActivityAware {
             }
         }
 
-        MgDataConnectivityStateHandler(context, flutterPluginBinding.binaryMessenger)
-        MgApStateHandler(context, flutterPluginBinding.binaryMessenger)
-    }
-
-    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-        channel.setMethodCallHandler(null)
-    }
-
-    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-        activity = binding.activity
+        MgDataConnectivityStateHandler(context, binaryMessenger)
+        MgApStateHandler(context, binaryMessenger)
     }
 
     override fun onDetachedFromActivityForConfigChanges() {
